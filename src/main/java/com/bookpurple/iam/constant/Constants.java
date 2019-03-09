ce9bfeae20af46954e7dcafcbfde6faa5856f542
gaurav4ever <gauravsharma.mvp@gmail.com>
2019-03-09 10:57:37 +0530

    open api interceptor added

---
+++ src/main/java/com/bookpurple/iam/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.bookpurple.iam.constant;
+
+/*
+ * Created by gauravsharma on 2019-03-09.
+ */
+public class Constants {
+
+    public static class SecurityConstants {
+        public static final String X_GBP_AUTH = "X-GBP-AUTH";
+        public static final String AUTHORIZATION = "Authorization";
+    }
+}
--- src/main/java/com/bookpurple/iam/controller/OtpController.java
+++ src/main/java/com/bookpurple/iam/controller/OtpController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping("/iam")
 public class OtpController {
 
     @Autowired
